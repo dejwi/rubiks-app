d3f887be23a990ae2ce19845445c3c34c6cf51ac
dejwi <misti.krasula@gmail.com>
2024-02-14 23:41:54 +0100

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-export const fontSans = Inter({
+const fontSans = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
--- app/tests/page.tsx
+++
@@ -1,5 +0,0 @@
-import TestsPage from "@/components/tests-page/tests-page";
-
-const Page = () => <TestsPage />;
-
-export default Page;
