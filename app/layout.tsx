9a658a4709996e16757837d9e6c51edb9d7aa6dc
dejwi <misti.krasula@gmail.com>
2024-03-03 22:34:21 +0100

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const fontSans = Inter({
   subsets: ["latin"],
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={cn("bg-background font-sans antialiased", fontSans.variable)}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
